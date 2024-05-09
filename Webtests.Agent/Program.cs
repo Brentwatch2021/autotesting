2d9e71c7454172f834584a3e2466c9450eabecb2
Brent Becker <57631700+Brentwatch2021@users.noreply.github.com>
2024-05-09 21:47:45 +0200

    Add files via upload

---
+++ Webtests.Agent/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Webtests;
+namespace Webtests.Agent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            HelloWorldTest test = new HelloWorldTest();
+            test.SayHelloToWorld();
+        }
+    }
+}
